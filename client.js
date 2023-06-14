b9254d6aa56b385051b3e20bdc80e028c8d5b6e8
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-13 22:54:27 -0400

    added a name command

--- client.js
+++ client.js
@@ -4,16 +4,19 @@ const net = require("net");
 const connect = function () {
 
   const conn = net.createConnection({
-    host: "localhost", 
+    host: "localhost",
     port:  50541
   });
   conn.on("connect", () => {
     console.log("Connestion established.");
+    conn.write("Name: ATZ");
   });
+  conn.setEncoding("utf8");
+  
   conn.on("data", (data) => {
     console.log("Received data", data);
   });
-  conn.setEncoding("utf8");
+  
 
   return conn;
 };
