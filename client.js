b45f1e69239a18911993b87c43e0a26a5b87b51c
andrijana85 <topalovic.andrijana@yahoo.com>
2023-06-14 22:36:04 -0400

    added Constants Module

--- client.js
+++ client.js
@@ -1,17 +1,20 @@
 const net = require("net");
-
+const { IP, PORT } = require("./constants");
 
 const connect = function() {
 
   const conn = net.createConnection({
-    host: "localhost",
-    port:  50541
+    host: IP,
+    port:  PORT
   });
   conn.on("connect", () => {
     console.log("Connestion established.");
     conn.write("Name: ATZ");
+
     let index = 0;
+
     const moves = ["Move: up", "Move: up","Move: up","Move: up"];
+
     const interval = setInterval(() => {
       if (index < moves.length) {
         conn.write(moves[index]);
@@ -21,29 +24,13 @@ const connect = function() {
       }
     }, 50);
   });
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // },50);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // },100);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // },150);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // },200);
-  // setTimeout(() => {
-  //   conn.write("Move: up");
-  // },250);
-   
+
   conn.setEncoding("utf8");
   
   conn.on("data", (data) => {
     console.log("Received data", data);
   });
   
-
   return conn;
 };
 const setupInput = function() {
